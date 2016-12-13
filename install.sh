37c2b5a6e634f2ea112603fcee6c8c2d7a3cad6a
Ruijian Cao <cao7113@hotmail.com>
2016-12-13 16:05:22 +0800

    fix sudo on docker

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-# Usage:
-#  $0 [aliyun]
+# Usage: $0 [aliyun]
+
+sudo_cmd=''
+which sudo &> /dev/null && sudo_cmd='sudo'
 
 function bakup_once(){
   raw=$1
   suffix=capt.bak
   target=$raw.$suffix
   [ -e "$target" ] || {
-    sudo mv $raw $target
+    $sudo_cmd mv $raw $target
   } 
 }
 
@@ -22,10 +24,16 @@ tag=${version//.}
 if [ $# -gt 0 ]; then
   tag=$tag-$1
 fi
-url=${SOURCE_URL:-https://github.com/cao7113/capt/raw/master/share/${tag}-source}
-echo == get source from: $url
 
-sudo wget -O /etc/apt/sources.list $url
-sudo apt-get update
+lfile=$(dirname $0)/share/${tag}-source
+if [ -e $lfile ]; then
+  $sudo_cmd cp -f $lfile /etc/apt/sources.list
+else
+  url=${SOURCE_URL:-https://github.com/cao7113/capt/raw/master/share/${tag}-source}
+  echo == get source from: $url
+  $sudo_cmd wget -O /etc/apt/sources.list $url
+fi
+
+$sudo_cmd apt-get -y update
 
 echo ==finished change sources
